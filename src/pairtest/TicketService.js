3ecba6d5cbe5b6ce89732505c7bd2eb3226dcb95
Luke Wilde <lukew@serenityos.org>
2022-11-25 01:14:52 +0000

    Validate ticket requests

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -18,5 +18,55 @@ export default class TicketService {
 
     if (accountId <= 0)
       throw new InvalidPurchaseException("Account ID must be greater than zero");
+
+    // Business rules:
+    // - Multiple tickets can be purchased at any given time.
+    // - Only a maximum of 20 tickets that can be purchased at a time.
+
+    if (ticketTypeRequests.length === 0)
+      throw new InvalidPurchaseException("Must make at least one ticket type request");
+
+    // This assumes that each TicketTypeRequest has `noOfTickets` set to at least 1, which will be validated later on. This saves iterating over the requests in this case.
+    if (ticketTypeRequests.length > 20)
+      throw new InvalidPurchaseException("Only a maximum of 20 tickets can be purchased at a time");
+
+    // Business rule:
+    // - There are 3 types of tickets i.e. Infant, Child, and Adult.
+    // This is used to reconstuct the total number of tickets per ticket type.
+    // For example, ticketTypeRequests could contain a single TicketTypeRequest with `type` "ADULT" and `noOfTickets` set to 5,
+    // or 5 TicketTypeRequest objects with `type` "ADULT" and `noOfTickets` set to 1.
+    const ticketTypeTotals = {
+      // This relies on TicketTypeRequest already validating the type, as it can't be constructed with an invalid type.
+      INFANT: 0,
+      CHILD: 0,
+      ADULT: 0,
+    };
+
+    // Business rule:
+    // - The ticket purchaser declares how many and what type of tickets they want to buy.
+    for (const request of ticketTypeRequests) {
+      if (!(request instanceof TicketTypeRequest))
+        throw new InvalidPurchaseException("The ticket requests contain a non-request value");
+
+      const numberOfTickets = request.getNoOfTickets();
+
+      // TicketTypeRequest has already validated that numberOfTickets is an integer, as it can't be constructed otherwise, so we only have to check its range here.
+      if (numberOfTickets <= 0)
+        throw new InvalidPurchaseException("A ticket request is requesting zero or less tickets");
+
+      // This relies on TicketTypeRequest already validating the type, as it can't be constructed with an invalid type.
+      ticketTypeTotals[request.getTicketType()] += numberOfTickets;
+    }
+
+    const totalAmountOfTickets = ticketTypeTotals.INFANT + ticketTypeTotals.CHILD + ticketTypeTotals.ADULT;
+
+    // We don't have to check if there's zero or less tickets here, as we would have thrown above if a request had zero or less tickets.
+    if (totalAmountOfTickets > 20)
+      throw new InvalidPurchaseException("Only a maximum of 20 tickets can be purchased at a time");
+
+    // Business rule:
+    // - Child and Infant tickets cannot be purchased without purchasing an Adult ticket.
+    if (ticketTypeTotals.ADULT === 0 && (ticketTypeTotals.INFANT > 0 || ticketTypeTotals.CHILD > 0))
+      throw new InvalidPurchaseException("Child and Infant tickets cannot be purchased without purchasing an Adult ticket");
   }
 }
--- test/TicketService.test.js
+++ test/TicketService.test.js
@@ -4,7 +4,7 @@ import TicketTypeRequest from "../src/pairtest/lib/TicketTypeRequest";
 const ticketService = new TicketService();
 
 describe("valid purchase requests", () => {
-    test("valid account id", () => {
+    test("valid account id and ticket request", () => {
         const testValues = [
             1,
             2,
@@ -21,28 +21,54 @@ describe("valid purchase requests", () => {
     });
 });
 
-describe("invalid purchase requests", () => {
-    test("invalid account id type", () => {
-        const testValues = [
-            undefined,
-            null,
-            false,
-            true,
-            1.1,
-            NaN,
-            Infinity,
-            -Infinity,
-            "test",
-            1n,
-            () => {},
-            {},
-            { valueOf() { return 1; } },
-            [],
-            [1],
-            Symbol.toPrimitive,
-        ];
+const invalidTestValues = [
+    undefined,
+    null,
+    false,
+    true,
+    1.1,
+    NaN,
+    Infinity,
+    -Infinity,
+    "test",
+    1n,
+    () => {},
+    {},
+    { valueOf() { return 1; } },
+    [],
+    [1],
+    Symbol.toPrimitive,
+];
+
+describe("invalid ticket type requests", () => {
+    test("invalid type", () => {
+        const testValues = invalidTestValues.concat("", "Adult", "Child", "Infant");
 
         for (const value of testValues) {
+            expect(() => {
+                new TicketTypeRequest(value, 1);
+            }).toThrow("type must be ADULT, CHILD, or INFANT");
+        }
+    });
+
+    test("number of tickets must be an integer", () => {
+        for (const value of invalidTestValues) {
+            expect(() => {
+                new TicketTypeRequest("ADULT", value);
+            }).toThrow("noOfTickets must be an integer");
+        }
+    });
+});
+
+describe("invalid purchase requests", () => {
+    const testValuesForIntegersAboveZero = [
+        0,
+        -0,
+        -1,
+    ];
+
+    test("invalid account id type", () => {
+        for (const value of invalidTestValues) {
             expect(() => {
                 ticketService.purchaseTickets(value);
             }).toThrow("Account ID is not an integer");
@@ -54,16 +80,88 @@ describe("invalid purchase requests", () => {
     });
 
     test("invalid account id range", () => {
-        const testValues = [
-            0,
-            -0,
-            -1,
-        ];
-
-        for (const value of testValues) {
+        for (const value of testValuesForIntegersAboveZero) {
             expect(() => {
                 ticketService.purchaseTickets(value);
             }).toThrow("Account ID must be greater than zero");
+        }
+    });
+
+    test("must make at least one ticket type request", () => {
+        expect(() => {
+            ticketService.purchaseTickets(1);
+        }).toThrow("Must make at least one ticket type request");
+    });
+
+    test("can only purchase 20 tickets at a time", () => {
+        const requests = [];
+        for (let i = 0; i <= 20; i++) {
+            requests.push(new TicketTypeRequest("ADULT", 1));
+        }
+
+        expect(() => {
+            ticketService.purchaseTickets(1, ...requests);
+        }).toThrow("Only a maximum of 20 tickets can be purchased at a time");
+
+        // Doesn't depend on type
+        expect(() => {
+            ticketService.purchaseTickets(1, new TicketTypeRequest("ADULT", 21));
+        }).toThrow("Only a maximum of 20 tickets can be purchased at a time");
+
+        expect(() => {
+            ticketService.purchaseTickets(1, new TicketTypeRequest("INFANT", 21));
+        }).toThrow("Only a maximum of 20 tickets can be purchased at a time");
+
+        expect(() => {
+            ticketService.purchaseTickets(1, new TicketTypeRequest("CHILD", 21));
+        }).toThrow("Only a maximum of 20 tickets can be purchased at a time");
+
+        // Mixture of types
+        expect(() => {
+            ticketService.purchaseTickets(1, new TicketTypeRequest("ADULT", 10), new TicketTypeRequest("CHILD", 10), new TicketTypeRequest("INFANT", 1));
+        }).toThrow("Only a maximum of 20 tickets can be purchased at a time");
+
+        // Multiple of same type spread across different requests.
+        expect(() => {
+            ticketService.purchaseTickets(1, new TicketTypeRequest("ADULT", 10), new TicketTypeRequest("ADULT", 10), new TicketTypeRequest("ADULT", 1));      
+        }).toThrow("Only a maximum of 20 tickets can be purchased at a time");
+
+        expect(() => {
+            ticketService.purchaseTickets(1, new TicketTypeRequest("INFANT", 10), new TicketTypeRequest("INFANT", 10), new TicketTypeRequest("INFANT", 1));      
+        }).toThrow("Only a maximum of 20 tickets can be purchased at a time");
+
+        expect(() => {
+            ticketService.purchaseTickets(1, new TicketTypeRequest("CHILD", 10), new TicketTypeRequest("CHILD", 10), new TicketTypeRequest("CHILD", 1));      
+        }).toThrow("Only a maximum of 20 tickets can be purchased at a time");
+    });
+
+    test("children/infants must be accompanied by an adult", () => {
+        expect(() => {
+            ticketService.purchaseTickets(1, new TicketTypeRequest("CHILD", 20));
+        }).toThrow("Child and Infant tickets cannot be purchased without purchasing an Adult ticket");
+
+        expect(() => {
+            ticketService.purchaseTickets(1, new TicketTypeRequest("INFANT", 20));
+        }).toThrow("Child and Infant tickets cannot be purchased without purchasing an Adult ticket");
+
+        expect(() => {
+            ticketService.purchaseTickets(1, new TicketTypeRequest("INFANT", 10), new TicketTypeRequest("CHILD", 10));
+        }).toThrow("Child and Infant tickets cannot be purchased without purchasing an Adult ticket");
+    });
+
+    test("requests must be of type TicketTypeRequest", () => {
+        for (const value of invalidTestValues) {
+            expect(() => {
+                ticketService.purchaseTickets(1, value);
+            }).toThrow("The ticket requests contain a non-request value");
+        }
+    });
+
+    test("ticket request cannot request zero or less tickets", () => {
+        for (const value of testValuesForIntegersAboveZero) {
+            expect(() => {
+                ticketService.purchaseTickets(1, new TicketTypeRequest("ADULT", value));
+            }).toThrow("A ticket request is requesting zero or less tickets");
         }
     });
 });
